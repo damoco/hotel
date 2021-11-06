dd721cc601d0aa2ec7c12cc49919286dcb7ca4d7
zhaolei <zhaolei@easyact.cn>
2021-11-06 20:39:03 +0800

    + add booking

--- app/src/main/java/hotel/model/HotelData.java
+++ app/src/main/java/hotel/model/HotelData.java
@@ -1,21 +1,22 @@
 package hotel.model;
 
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toUnmodifiableSet;
 
 public record HotelData(Set<Integer> rooms, Set<Booking> bookings) {
 
 	public HotelData withSize(int size) {
-		return new HotelData(IntStream.rangeClosed(1, size).boxed().collect(Collectors.toUnmodifiableSet()), bookings);
+		return new HotelData(IntStream.rangeClosed(1, size).boxed().collect(toUnmodifiableSet()), bookings);
 	}
 
 	public HotelData withBookings(Set<Booking> bookings) {
 		return new HotelData(rooms, bookings);
 	}
 
-//	public HotelData addBooking(Booking booking) {
-//		return new HotelData(rooms, bookings);
-//	}
+	public HotelData addBooking(Booking booking) {
+		return new HotelData(rooms, Stream.concat(bookings.stream(), Stream.of(booking)).collect(toUnmodifiableSet()));
+	}
 }
--- app/src/main/java/hotel/module/Hotel.java
+++ app/src/main/java/hotel/module/Hotel.java
@@ -13,10 +13,9 @@ public interface Hotel {
 	static Set<Integer> findAvailableRoomsOn(HotelData data, LocalDate date) {
 		final Set<Integer> booked4Date = data.bookings().stream().filter(booking -> booking.date().equals(date))
 				.map(Booking::room).collect(toUnmodifiableSet());
-		final Set<Integer> allRooms = data.rooms();
-		final Set<Integer> r = new TreeSet<>(allRooms);
+		final Set<Integer> r = new TreeSet<>(data.rooms());
 		r.removeAll(booked4Date);
-		System.out.printf("findAvailableRoomsOn: %s, %s, %s%n", allRooms, booked4Date, r);
-		return r;
+		System.out.printf("findAvailableRoomsOn: %s, %s, %s%n", data.rooms(), booked4Date, r);
+		return Set.copyOf(r);
 	}
 }
