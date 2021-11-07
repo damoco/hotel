f0865caea8a83fac0059f4b60201d8cba2b9f9fe
zhaolei <zhaolei@easyact.cn>
2021-11-07 21:59:34 +0800

    - syncronize with AtomicReference

--- app/src/main/java/hotel/HotelController.kt
+++ app/src/main/java/hotel/HotelController.kt
@@ -4,37 +4,25 @@ import hotel.model.Booking
 import hotel.model.HotelData
 import hotel.service.Hotel.*
 import java.time.LocalDate
-import java.util.stream.Collectors
-import java.util.stream.Collectors.toUnmodifiableSet
-import java.util.stream.Stream
+import java.util.concurrent.atomic.AtomicReference
 
 class HotelController {
-	private var data = HotelData(emptySet(), emptySet())
+	private var data = AtomicReference(HotelData(emptySet(), emptySet()))
 
 	fun configRoomSize(size: Int) {
-		data = data.withSize(size)
+		data.getAndUpdate { it.withSize(size) }
 	}
 
 	fun setBookings(bookings: Set<Booking>) {
-		data = data.withBookings(bookings)
+		data.getAndUpdate { it.withBookings(bookings) }
 	}
 
-	fun findAvailableRoomsOn(date: LocalDate): Set<Int> = findAvailableRoomsOn(data, date)
+	fun findAvailableRoomsOn(date: LocalDate): Set<Int> = findAvailableRoomsOn(data.get(), date)
 
 	fun bookRoom(date: LocalDate, room: Int, guestName: String) {
-		val previousBookings = data.bookings
-		val nextData = bookRoom(data, date, room, guestName)
-		// Optimistic Concurrency Control
-		synchronized(this) {
-			if (previousBookings == data.bookings) {
-				data = nextData
-			} else if (data.bookings.none { it.date == date && it.room == room }) {
-				println("Will merge state: $nextData to $data")
-				data = data.withBookings(data.bookings + nextData.bookings)
-			} else bookingConflictException(date, room)
-		}
+		data.getAndUpdate { bookRoom(it, date, room, guestName) }
 		println("book result: $data")
 	}
 
-	fun bookingsByGuest(guestName: String): Set<Booking> = bookingsByGuest(data, guestName)
+	fun bookingsByGuest(guestName: String): Set<Booking> = bookingsByGuest(data.get(), guestName)
 }
