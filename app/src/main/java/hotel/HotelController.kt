79b2eeffae4bb9da5dfea226a21b4466d812d65a
zhaolei <zhaolei@easyact.cn>
2021-11-18 11:34:49 +0800

    - DOP by OO service

--- app/src/main/java/hotel/HotelController.kt
+++ app/src/main/java/hotel/HotelController.kt
@@ -1,33 +1,16 @@
 package hotel
 
 import hotel.model.Booking
-import hotel.model.HotelData
-import hotel.service.Hotel.*
+import hotel.service.HotelService
 import java.time.LocalDate
-import java.util.concurrent.atomic.AtomicReference
 
-class HotelController(size: Int) {
-	private var current = AtomicReference(HotelData(size, emptySet()))
+class HotelController(size: Int, private val service: HotelService = HotelService(size)) {
 
-	fun setBookings(bookings: Set<Booking>) {
-		current.updateAndGet { it.withBookings(bookings) }
-	}
+	fun setBookings(bookings: Set<Booking>) = service.setBookings(bookings)
 
-	fun findAvailableRoomsOn(date: LocalDate): Set<Int> = findAvailableRoomsOn(current.get(), date)
+	fun findAvailableRoomsOn(date: LocalDate): Set<Int> = service.findAvailableRoomsOn(date)
 
-	fun bookRoom(date: LocalDate, room: Int, guestName: String) {
-		val previous = current.get()
-		val next = bookRoom(previous, date, room, guestName)
-//		current.set(next)// 并发booking会冲突
-		current.updateAndGet {
-			if (it == previous) next
-			else if (it.bookings.none { booking -> booking.date == date && booking.room == room }) {
-//				println("Will merge next: $next to current: $it")
-				it.withBookings(it.bookings + next.bookings)
-			} else throw bookingConflictException(date, room)
-		}
-//			.also { println("book result: $current") }
-	}
+	fun bookRoom(date: LocalDate, room: Int, guestName: String) = service.bookRoom(date, room, guestName)
 
-	fun bookingsByGuest(guestName: String): Set<Booking> = bookingsByGuest(current.get(), guestName)
+	fun bookingsByGuest(guestName: String): Set<Booking> = service.bookingsByGuest(guestName)
 }
--- app/src/main/java/hotel/service/Hotel.java
+++ app/src/main/java/hotel/service/Hotel.java
@@ -9,7 +9,7 @@ import java.util.TreeSet;
 
 import static java.util.stream.Collectors.toUnmodifiableSet;
 
-public interface Hotel {
+interface Hotel {
 	static Set<Integer> findAvailableRoomsOn(HotelData data, LocalDate date) {
 		final Set<Integer> booked4Date = data.bookings().stream().filter(booking -> booking.date().equals(date))
 				.map(Booking::room).collect(toUnmodifiableSet());
